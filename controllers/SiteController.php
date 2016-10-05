5dc75eec673eeaed7a950a73437436a660537dfc
stee1 <krdima92@gmail.com>
2016-10-05 10:39:36 +0300

    deleted basic yii2 pages

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -134,6 +134,11 @@ class SiteController extends Controller
         ]);
     }
 
+    /**
+     * Record action.
+     *
+     * @return string
+     */
     public function actionRecord()
     {
         $model = new CommentsForm();
@@ -179,65 +184,5 @@ class SiteController extends Controller
             'current_record' => $current_record,
             'comments' => $comments,
         ]);
-    }
-
-    /**
-     * Login action.
-     *
-     * @return string
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return string
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
-
-    /**
-     * Displays contact page.
-     *
-     * @return string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
     }
 }
